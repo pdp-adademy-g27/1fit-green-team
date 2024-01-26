1836f4fdfc27323fc32f5bed3180ba182940a279
Qod1rc1k <abduqodirzuvaydullayev@gmail.com>
2024-01-26 16:39:40 +0500

    Rating CRUD

--- src/main/java/com/example/onefit/rating/dto/RatingResponseDto.java
+++ src/main/java/com/example/onefit/rating/dto/RatingResponseDto.java
@@ -1,33 +1,21 @@
-package com.example.onefit.rating.entity;
+package com.example.onefit.rating.dto;
 
 import com.example.onefit.course.entity.Course;
 import com.example.onefit.user.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "`rating`")
-public class Rating {
-
-    @Id
+@NoArgsConstructor
+public class RatingResponseDto {
     private UUID id;
     private Integer star;
     private String comment;
-    @ManyToOne
     private User user;
-    @ManyToOne
     private Course course;
-
-
-
-
-
-
-
 
 }
