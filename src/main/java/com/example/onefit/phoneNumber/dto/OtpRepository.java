0217f83b2c06b6c5cb9b306604a51a98c3b1dd2b
shuhrathaqnazarov <139967374+shuhrathaqnazarov@users.noreply.github.com>
2024-01-31 17:10:15 +0500

    otp  delete

--- src/main/java/com/example/onefit/phoneNumber/dto/OtpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.onefit.phoneNumber.dto;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OtpRepository extends CrudRepository<Otp, String> {
-}
