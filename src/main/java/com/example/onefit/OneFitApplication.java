fde095089c5d0b26d0cab1828a3a382d401fb4bc
developer <mavjudbeksokhibov@gmail.com>
2024-02-01 08:59:54 +0500

    oshiqcha importlar ochrildi

--- src/main/java/com/example/onefit/OneFitApplication.java
+++ src/main/java/com/example/onefit/OneFitApplication.java
@@ -1,20 +1,9 @@
 package com.example.onefit;
 
-import com.example.onefit.user.UserRepository;
-import com.example.onefit.user.entity.User;
-import com.example.onefit.user.role.RoleRepository;
-import com.example.onefit.user.role.entiy.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Set;
-import java.util.UUID;
 
 @SpringBootApplication
 @RequiredArgsConstructor
